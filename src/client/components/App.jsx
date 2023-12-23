5c85af5c4b418a5e76024282059166e1364c3e28
Tanner Robertson <tanman1811@gmail.com>
2023-12-23 12:21:04 -0700

    Joseph's style changes

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './styles.scss';
+import FooterButtons from './footerButtons.js';
 
 function App() {
   return (
-  <div className="container">
-    <h3>Welcome to</h3>
-    <h1>StalkStocks</h1>
-  </div>
-    )
+    <div>
+      <div className="container">
+        <h1 className="app-title">StalkStocks</h1>
+      </div>
+      <div className="search-container">
+        <div className="search-center">
+          <form className="search-form">
+            <input type="text" className="search-box" />
+            <button type="submit" className="search-button">
+              Stalk
+            </button>
+          </form>
+        </div>
+      </div>
+      <FooterButtons />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/client/components/footerButtons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FooterButtons() {
+  return (
+    <div className="footer-buttons">
+      <button className="footer-button">Meet the team</button>
+      <button className="footer-button">Our mission</button>
+    </div>
+  );
+}
+
+export default FooterButtons;
