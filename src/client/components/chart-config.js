71653393789c6feae4e59c00f306a0c9b0975291
apalazzomusic <apalazzomusic@gmail.com>
2023-12-26 13:18:23 -0500

    changes y-axis to ignore zero bound

--- src/client/components/chart-config.js
+++ src/client/components/chart-config.js
@@ -65,7 +65,7 @@ async function fetchDataFromAPI() {
       options: {
         scales: {
           y: {
-            beginAtZero: true
+            beginAtZero: false
           }
         }
       }
